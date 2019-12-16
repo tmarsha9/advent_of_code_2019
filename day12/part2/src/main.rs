9cf59c5e407395c9db2926e99afb210774744556
Tyler Marshall <ty.e.marshall@gmail.com>
2019-12-15 20:37:21 -0600

    Add day12 solution
    Definitely needed help on part 2.  Took insights from https://work.njae.me.uk/2019/12/13/advent-of-code-2019-day-12/

---
+++ day12/part2/src/main.rs
@@ -0,0 +1,223 @@
+use std::fs::File;
+use std::io::{BufRead,BufReader};
+use std::error::Error;
+use num::integer::{lcm, Integer};
+
+#[derive(Debug, Clone)]
+struct Position {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Position {
+    fn add_velocity(&mut self, v: &Velocity) {
+        self.x += v.x;
+        self.y += v.y;
+        self.z += v.z;
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Velocity {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Velocity {
+    fn add_acceleration(&mut self, a: &Acceleration) {
+        self.x += a.x;
+        self.y += a.y;
+        self.z += a.z;
+    }
+}
+
+#[derive(Debug)]
+struct Acceleration {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+#[derive(Debug, Clone)]
+struct Moon {
+    p: Position,
+    v: Velocity,
+}
+
+struct System {
+    initial_moons: Vec<Moon>,
+    moons: Vec<Moon>,
+}
+
+impl System {
+    fn new() -> Self{
+        System {
+            initial_moons: Vec::new(),
+            moons: Vec::new()
+        }
+    }
+
+    fn tick(&mut self) {
+        // update velocities
+
+        // need the accelerations vector because can't have a mutable iter and an immutable
+        // iterator to moons vec
+        let mut accelerations = Vec::new();
+
+        for (i, moon) in self.moons.iter().enumerate() {
+            let mut a = Acceleration {x: 0, y: 0, z: 0};
+
+            for (j, moon2) in self.moons.iter().enumerate() {
+                if i == j {
+                    // skip self
+                    continue;
+                } else {
+                    // compute acceleration that affects moon, NOT moon2
+                    // moon2 will be handled by other loop and it will be labeled just moon then
+                    let dx = moon2.p.x-moon.p.x;
+                    let dy = moon2.p.y-moon.p.y;
+                    let dz = moon2.p.z-moon.p.z;
+
+                    // acceleration only affected by at most +/- 1
+                    if dx < 0 {
+                        a.x += -1;
+                    } else if dx > 0 {
+                        a.x += 1;
+                    }
+                    if dy < 0 {
+                        a.y += -1;
+                    } else if dy > 0 {
+                        a.y += 1;
+                    }
+                    if dz < 0 {
+                        a.z += -1;
+                    } else if dz > 0 {
+                        a.z += 1;
+                    }
+                }
+            }
+            accelerations.push(a);
+        }
+
+        // modify velocities after computing acceleration for each
+        for (a, moon) in accelerations.iter().zip(self.moons.iter_mut()) {
+            moon.v.add_acceleration(&a);
+        }
+
+        // update positions
+        for moon in self.moons.iter_mut() {
+            moon.p.add_velocity(&moon.v);
+        }
+    }
+
+    fn check_x(&self) -> bool {
+        // check if x positions match initial
+        for (i, moon) in self.moons.iter().enumerate() {
+            let initial_moon = &self.initial_moons[i];
+            if moon.p.x != initial_moon.p.x || moon.v.x != initial_moon.v.x {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    fn check_y(&self) -> bool {
+        // check if x positions match initial
+        for (i, moon) in self.moons.iter().enumerate() {
+            let initial_moon = &self.initial_moons[i];
+            if moon.p.y != initial_moon.p.y || moon.v.y != initial_moon.v.y {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    fn check_z(&self) -> bool {
+        // check if x positions match initial
+        for (i, moon) in self.moons.iter().enumerate() {
+            let initial_moon = &self.initial_moons[i];
+            if moon.p.z != initial_moon.p.z || moon.v.z != initial_moon.v.z {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    fn compute_period(&mut self) -> u64 {
+        let mut period_x: Option<u64> = None;
+        let mut period_y: Option<u64> = None;
+        let mut period_z: Option<u64> = None;
+        let mut tick_counter = 0u64;
+        let mut finished;
+
+        self.moons = self.initial_moons.clone();
+        loop {
+            finished = true;
+            self.tick();
+            tick_counter += 1;
+            if let None = period_x {
+                finished = false;
+                if self.check_x() {
+                    period_x = Some(tick_counter);
+                }
+            }
+            if let None = period_y {
+                finished = false;
+                if self.check_y() {
+                    period_y = Some(tick_counter);
+                }
+            }
+            if let None = period_z {
+                finished = false;
+                if self.check_z() {
+                    period_z = Some(tick_counter);
+                }
+            }
+
+            if finished {
+                break;
+            }
+        }
+
+
+        let period_x = period_x.unwrap();
+        let period_y = period_y.unwrap();
+        let period_z = period_z.unwrap();
+
+        period_x.lcm(&period_y.lcm(&period_z))
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let f = File::open("../input.txt")?;
+    let f = BufReader::new(f);
+
+    let mut s = System::new();
+
+    for line in f.lines() {
+        let line = line?.split(',').map(|mut s| {
+            s = s.trim_start_matches("<");
+            s = s.trim_start_matches("x=");
+            s = s.trim_start_matches(" y=");
+            s = s.trim_start_matches(" z=");
+            s = s.trim_end_matches(">");
+            s.parse().unwrap()
+        }).collect::<Vec<i32>>();
+        let x = line[0];
+        let y = line[1];
+        let z = line[2];
+
+        let p = Position {x,y,z};
+        let v = Velocity {x: 0, y: 0, z: 0};
+        let m = Moon{p,v};
+        s.initial_moons.push(m);
+    }
+
+    println!("{}", s.compute_period());
+    Ok(())
+}
